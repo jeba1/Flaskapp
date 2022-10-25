745cc79ff55acc9319aed46a57a7c30c6ce682e5
jeba1 <jebasafa@gmail.com>
2022-10-24 22:48:44 -0500

    second

---
+++ user_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo yum -y update
+sudo yum install git -y
+sudo yum install python3 -y
+sudo yum install python3-pip
+sudo git clone https://github.com/jeba1/Flaskapp.git
+cd Flaskapp
+pip3 install -r requirements.txt
+cp app.py requirements.txt test_candidates.py ~/app/src
+export TC_DYNAMO_TABLE=Candidates
+cp gunicon /etc/systemd/system/gunicorn.service
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn-
+sudo systemctl enable gunicorn
+
