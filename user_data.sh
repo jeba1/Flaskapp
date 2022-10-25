ea5e523f7532a11d76f8e2c270e95d87e112af23
jeba1 <jebasafa@gmail.com>
2022-10-24 23:16:49 -0500

    third

--- user_data.sh
+++ user_data.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 sudo yum -y update
 sudo yum install git -y
 sudo yum install python3 -y
@@ -6,10 +6,13 @@ sudo yum install python3-pip
 sudo git clone https://github.com/jeba1/Flaskapp.git
 cd Flaskapp
 pip3 install -r requirements.txt
+mkdir -p ~/app/src
 cp app.py requirements.txt test_candidates.py ~/app/src
 export TC_DYNAMO_TABLE=Candidates
-cp gunicon /etc/systemd/system/gunicorn.service
+sudo cp gunicorn /etc/systemd/system/gunicorn.service
 sudo systemctl daemon-reload
-sudo systemctl start gunicorn-
+
+sudo systemctl start gunicorn
+
 sudo systemctl enable gunicorn
 
